4a163256a0ad81c7a42c52e728120c792d45a9b9
Vladimir Vovk <57999167+VladimirVovk97@users.noreply.github.com>
2021-11-24 14:18:20 +0200

    фикс

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './sass/main.scss';
 
 //main
-// import './.partials/js/card-to-list';
+import './.partials/js/card-to-list';
 
 //footer
-//import './.partials/team-list.json';
-//import './.partials/js/modal-team-list';
+import './.partials/team-list.json';
+import './.partials/js/modal-team-list';
