50ba3327aa9261882f0f75adcd2a4d0527f2e987
mezentsx <mezentsxn@gmail.com>
2021-11-29 21:15:04 +0100

    Добавила кнопку, завтра подкорректирую и кнопку и пагинацию

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import './.partials/team-list.json';
 import './.partials/js/modal-team-list';
 import './.partials/js/markup-films';
 import './.partials/js/modal-film-description';
+import './.partials/js/btnUp'
 import './sass/main.scss';
 
